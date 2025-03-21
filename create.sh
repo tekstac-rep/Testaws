197aeba2022f2bf7c2409a388019126ac641532f
tekstac-rep <salesforce-lab1@teknoturf.net>
2025-03-21 09:55:58 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -6,6 +6,7 @@ terraform init
 ls
 terraform apply -var "access_key=$1" -var "secret_key=$2" -auto-approve > terraform.log
 ls
+sleep 50s
 # Check if Terraform execution was successful
 if grep -q "Apply complete! Resources: .* added, 0 changed, 0 destroyed." terraform.log; then
     echo "Success"
